5b3635405149e841eb643215c64164219a2595a4
EliCash82 <AlmostAnderson@gmail.com>
2013-07-29 09:53:21 -0400

    Added intro line from novel

--- caleb.py
+++ caleb.py
@@ -5,7 +5,7 @@ def library():
     print "There are several books you've been meaning to pick up here."
     print "How many do you take?"
     
-    next = raw_input(">>>")
+    next = raw_input(">")
     if "0" in next or "1" in next:
         how_much = int(next)
     else:
@@ -29,15 +29,28 @@ def dead(why):
 def start():
     print "Welcome to PROTO Experiment 1:"
     print ""
+    print ""
+    print ""
+    print ""
     print "Things as They Are; or,"
     print "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+    print ""
     print "                                    THE ADVENTURES OF...                                                       "
     print "_______ _______        _______ ______                 _  _  _ _____               _____ _______ _______ _______"
     print "|       |_____| |      |______ |_____]                |  |  |   |   |      |        |   |_____| |  |  | |______"
     print "|_____  |     | |_____ |______ |_____]                |__|__| __|__ |_____ |_____ __|__ |     | |  |  | ______|"
     print ""
+    print ""
     print "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
     print "                                                  A Game by Anderson Evans based on the novel by William Godwin"
+    print ""
+    print ""
+    print "                                                  My  life has for several years been a theatre of calamity.  I"
+    print "                                                  have  been  a mark for the vigilance of tyranny, and I  could"
+    print "                                                  not escape.  My fairest prospects have been blasted. My enemy"
+    print "                                                  has shown himself inaccessible to entreaties,  and untired in"
+    print "                                                  persecution. My fame, as well as my happiness, has become his"    
+    print "                                                  victim.                                       -Caleb Williams"    
     print ""                                                                                                                
     print "This game is based on the 1794 novel by William Godwin."
     print "Your name is Caleb Williams, you work on the large estate of Ferdinando Fakland.  When you began your work here Fakland seemed like the best of men, however a twisted temper has begun to show at odd intervals."
